15ce7ec1b28a865884f510338347b4af60599979
makii <makii>
2019-02-13 10:36:57 +0900

    はじめてのコミット

--- git_learn/git_learn/No_25.cpp
+++ git_learn/git_learn/No_25.cpp
@@ -3,11 +3,11 @@
 //漢字表記の名前を返してください。
 const char* GetKanjiName_25()
 {
-	return "山田　太郎";
+	return "槙井　陸人";
 }
 
 //ひらがな表記の名前を返してください。
 const char* GetHiraganaName_25()
 {
-	return "やまだ　たろう";
+	return "まきい　りくと";
 }
