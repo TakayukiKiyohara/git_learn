49657b6ba1288b1723777d2139ce95e0e1c9d5ff
kbc18b19 <kbc18b19@stu.kawahara.ac.jp>
2019-02-13 10:28:26 +0900

    初めてのコミット

--- git_learn/git_learn/No_19.cpp
+++ git_learn/git_learn/No_19.cpp
@@ -3,11 +3,11 @@
 //漢字表記の名前を返してください。
 const char* GetKanjiName_19()
 {
-	return "山田　太郎";
+	return "中村　速人";
 }
 
 //ひらがな表記の名前を返してください。
 const char* GetHiraganaName_19()
 {
-	return "やまだ　たろう";
+	return "なかむら　はやと";
 }
