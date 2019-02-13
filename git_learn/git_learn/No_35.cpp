f522d10ea651752ff77d1cb359419182eab8194d
takahasi <takahashimyu>
2019-02-13 10:48:00 +0900

    はじめてのコミット

--- git_learn/git_learn/No_35.cpp
+++ git_learn/git_learn/No_35.cpp
@@ -3,11 +3,11 @@
 //漢字表記の名前を返してください。
 const char* GetKanjiName_35()
 {
-	return "山田　太郎";
+	return "髙橋　美有";
 }
 
 //ひらがな表記の名前を返してください。
 const char* GetHiraganaName_35()
 {
-	return "やまだ　たろう";
+	return "たかはし　みゆう";
 }
