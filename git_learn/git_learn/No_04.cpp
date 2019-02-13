b3d2e7e3dfd8407b0e37def5479fd172907af7da
iwamoto <1>
2019-02-13 10:40:44 +0900

    はじめてのコッミト

--- git_learn/git_learn/No_04.cpp
+++ git_learn/git_learn/No_04.cpp
@@ -3,11 +3,11 @@
 //漢字表記の名前を返してください。
 const char* GetKanjiName_04()
 {
-	return "山田　太郎";
+	return "岩本　拓也";
 }
 
 //ひらがな表記の名前を返してください。
 const char* GetHiraganaName_04()
 {
-	return "やまだ　たろう";
+	return "いわもと　たくや";
 }
