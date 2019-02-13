6dea3b04269a7233db621b8690e6531add6c481b
kbc18b13 <kbc18b13@stu.kawahara.ac.jp>
2019-02-13 10:28:01 +0900

    初めてのコミット

--- git_learn/git_learn/No_13.cpp
+++ git_learn/git_learn/No_13.cpp
@@ -3,11 +3,11 @@
 //漢字表記の名前を返してください。
 const char* GetKanjiName_13()
 {
-	return "山田　太郎";
+	return "髙山　倫比古";
 }
 
 //ひらがな表記の名前を返してください。
 const char* GetHiraganaName_13()
 {
-	return "やまだ　たろう";
+	return "たかやま　みちひこ";
 }
