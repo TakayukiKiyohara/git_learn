b8edf71152e22ba854e982a8f6783b26388722a4
okamoto <1>
2019-02-13 10:42:49 +0900

    ”初めてのコミット

--- git_learn/git_learn/No_06.cpp
+++ git_learn/git_learn/No_06.cpp
@@ -3,11 +3,11 @@
 //漢字表記の名前を返してください。
 const char* GetKanjiName_06()
 {
-	return "山田　太郎";
+	return "岡山　秀";
 }
 
 //ひらがな表記の名前を返してください。
 const char* GetHiraganaName_06()
 {
-	return "やまだ　たろう";
+	return "おかやま　しゅう";
 }
