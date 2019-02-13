1e3362d052387b6064153f891d1cadcdc69e2d74
1 <1>
2019-02-13 12:20:37 +0900

    ぷいいいいぃぃぃp

--- git_learn/git_learn/No_18.cpp
+++ git_learn/git_learn/No_18.cpp
@@ -3,11 +3,11 @@
 //漢字表記の名前を返してください。
 const char* GetKanjiName_18()
 {
-	return "中野　響";
+	return "せいきんぶんぶん";
 }
 
 //ひらがな表記の名前を返してください。
 const char* GetHiraganaName_18()
 {
-	return "なかの　ぷいいい";
+	return "セイキンbunbun";
 }
