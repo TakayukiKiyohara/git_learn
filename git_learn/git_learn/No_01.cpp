6f05d2e52cc55c617d2f72fc429541af0572eefa
kbc18b01 <kbc18b01@stu.kawahara.ac.jp>
2019-02-13 10:28:04 +0900

    はじめてのコミット

--- git_learn/git_learn/No_01.cpp
+++ git_learn/git_learn/No_01.cpp
@@ -3,11 +3,11 @@
 //漢字表記の名前を返してください。
 const char* GetKanjiName_01()
 {
-	return "山田　太郎";
+	return "石川　雄基";
 }
 
 //ひらがな表記の名前を返してください。
 const char* GetHiraganaName_01()
 {
-	return "やまだ　たろう";
+	return "いしかわ　ゆうき";
 }
