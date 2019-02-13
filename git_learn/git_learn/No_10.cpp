ec7aa3989c996f8d3f40dd89cfecb480d119b0a3
近藤大翔 <1>
2019-02-13 10:42:08 +0900

    初めてのコミット

--- git_learn/git_learn/No_10.cpp
+++ git_learn/git_learn/No_10.cpp
@@ -3,11 +3,11 @@
 //漢字表記の名前を返してください。
 const char* GetKanjiName_10()
 {
-	return "山田　太郎";
+	return "近藤　大翔";
 }
 
 //ひらがな表記の名前を返してください。
 const char* GetHiraganaName_10()
 {
-	return "やまだ　たろう";
+	return "こんどう　ひろと";
 }
