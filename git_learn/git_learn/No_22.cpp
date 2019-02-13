712e46af2a800b8dd2b78ba08eea509bf9384b8b
GC1822 <1>
2019-02-13 11:25:11 +0900

    初めてのコミット

--- git_learn/git_learn/No_22.cpp
+++ git_learn/git_learn/No_22.cpp
@@ -3,11 +3,11 @@
 //漢字表記の名前を返してください。
 const char* GetKanjiName_22()
 {
-	return "山田　太郎";
+	return "飛田容疑者";
 }
 
 //ひらがな表記の名前を返してください。
 const char* GetHiraganaName_22()
 {
-	return "やまだ　たろう";
+	return "飛田容疑者";
 }
