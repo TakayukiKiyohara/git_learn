3f4256f2127e03fd9299c68078569c7a8f42ecea
TakayukiKiyohara <TakayukiKiyohara@gmail.com>
2019-02-13 11:41:21 +0900

    No_00.cppを追加

---
+++ git_learn/git_learn/No_00.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+
+//漢字表記の名前を返してください。
+const char* GetKanjiName_00()
+{
+	return "ホゲホゲ　ホゲホゲ";
+}
+
+//ひらがな表記の名前を返してください。
+const char* GetHiraganaName_00()
+{
+	return "ほげほげ　ほげほげ";
+}
