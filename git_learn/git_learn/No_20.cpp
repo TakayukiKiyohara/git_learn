858a78ecbc87fedabe560098ced51af92f711093
TakayukiKiyohara <TakayukiKiyohara@gmail.com>
2019-02-13 10:27:34 +0900

    初めてのコミット

--- git_learn/git_learn/No_20.cpp
+++ git_learn/git_learn/No_20.cpp
@@ -3,11 +3,11 @@
 //漢字表記の名前を返してください。
 const char* GetKanjiName_20()
 {
-	return "山田　太郎";
+	return "清原　隆行";
 }
 
 //ひらがな表記の名前を返してください。
 const char* GetHiraganaName_20()
 {
-	return "やまだ　たろう";
+	return "きよはら　たかゆき";
 }
